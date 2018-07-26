eed2e75844dcab5113f55f7023b6cff403328610
mrHolzed <solopov@MacBook-Pro-Konstantin.local>
2018-07-26 12:24:23 +0300

    link

--- js/main.js
+++ js/main.js
@@ -14,13 +14,13 @@ $(document).ready(function(){
 
   $(window).scroll(function(){
     if (window.pageYOffset > 300) {
-      $('.logo').css('height','60px').attr('src','../img/solopov-white.svg');
+      $('.logo').css('height','60px').attr('src','img/solopov-white.svg');
       $('header').css('height','60px').css('background-color','black');
       $('.navigation .language button').css('color','white').css('border-color','white');
       $('.sandwich').children().css('background-color','white');
       $('.language button').addClass('scroll-active');
     } else {
-      $('.logo').attr('src','../img/solopov.svg').css('height','100px');
+      $('.logo').attr('src','img/solopov.svg').css('height','100px');
       $('header').css('height','100px').css('background-color','rgba(0,0,0,.2)');
       $('.navigation .language button').css('color','black').css('border-color','black');
       $('.sandwich').children().css('background-color','black');
